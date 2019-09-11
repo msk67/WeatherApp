0d3ea019dc337d07ade9d8cc79311bc6a4240bbe
Min Kim <minkim@Mins-MacBook-Pro.local>
2019-09-11 12:30:16 -0500

    first project

---
+++ WeatherApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeatherApp
+//
+//  Created by Min Kim on 9/11/19.
+//  Copyright © 2019 Min Kim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
